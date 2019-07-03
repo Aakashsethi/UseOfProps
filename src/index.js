fc9594b21e082f2a6b1583169a31e2564ee0ad2c
Aakash Sethi <as3793@njit.edu>
2019-07-02 23:39:32 -0700

    Display Season on screen

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={lat:null, errorMessage:''};
-
-    
+    state = {lat:null, errorMessage:''};
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat:position.coords.latitude })
-            },
-            err =>{
-                this.setState({errorMessage:err.message})
-            }
-        );
+        position => this.setState({lat:position.coords.latitude }),
+        err => this.setState({errorMessage:err.message}),
+        
+    );
+
     }
+
 render()
    {
         
@@ -25,7 +22,7 @@ render()
         }
 
         if (!this.state.errorMessage && this.state.lat){
-            return <div>latitude:{this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat}/>
         }
         
         return <div>Loading</div>;
